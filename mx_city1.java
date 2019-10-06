3e62849d26274c523114f13908643faa05b51acf
Alex Xu <mxu@linux1.xingtech.com>
2019-10-06 12:01:19 -0400

    add

---
+++ mx_city1.java
@@ -0,0 +1,21 @@
+class Country {
+    String name;
+    void value() {
+       name = "China";
+    }
+}
+  
+class City extends Country {
+    String name;
+    void value() {
+    name = "Shanghai";
+    super.value();      // call father classe:
+    System.out.println(name);
+    System.out.println(super.name);
+    }
+  
+    public static void main(String[] args) {
+       City c=new City();
+       c.value();
+       }
+}
